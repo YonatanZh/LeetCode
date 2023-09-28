b6e94bb9d2dbc67c404458ac39dd4b080e9b054c
YonatanZh <91670596+YonatanZh@users.noreply.github.com>
2023-09-28 13:39:04 +0300

    Time: 1497 ms (33.26%), Space: 16.11 MB (99.22%)

--- 3Sum/three_sum.py
+++ 3Sum/three_sum.py
@@ -6,26 +6,26 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        j = 0
-        k = 0
+        l = 0
+        r = 0
         unique_tirplets = set()
 
         for i in range(len(nums) - 2):
             if nums[i] > TARGET:
                 break
-            j = i + 1
-            k = len(nums) - 1
-            while (j < k):
-                sum = nums[i] + nums[j] + nums[k]
+            l = i + 1
+            r = len(nums) - 1
+            while (l < r):
+                sum = nums[i] + nums[l] + nums[r]
                 if sum == TARGET:
-                    triplet = (nums[i], nums[j], nums[k])
+                    triplet = (nums[i], nums[l], nums[r])
                     if triplet not in unique_tirplets:
                         unique_tirplets.add((triplet))
-                    j += 1
-                    k -= 1
+                    l += 1
+                    r -= 1
                 elif sum < TARGET:
-                    j += 1
+                    l += 1
                 else:
-                    k -= 1
+                    r -= 1
 
         return list(unique_tirplets)
