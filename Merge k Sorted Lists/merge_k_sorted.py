9cda68eafcd5ce6432211a55f5c7243a0e77f5f1
YonatanZh <91670596+YonatanZh@users.noreply.github.com>
2023-10-17 21:08:21 +0300

    Time:  70 ms (76.18%), Space: 19.2 MB (95.88%)

---
+++ Merge k Sorted Lists/merge_k_sorted.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if lists == []:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+
+        return self.merge_two(left, right)
+    
+    def merge_two(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+
+
+        head = None
+        if list1.val <= list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+        
+        curr = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                curr = curr.next
+                list1 = list1.next
+            else:
+                curr.next = list2
+                curr = curr.next
+                list2 = list2.next
+        
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return head
